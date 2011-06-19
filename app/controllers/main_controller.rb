2e75146fc216811a6240ce0321029f1be7512d25
Scott Silverman <scottsil@me.com>
2011-06-19 08:55:43 -0700

    Ensure proper ordering

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ class MainController < ApplicationController
   def index
     # TODO: Add authentication logic
     @user = User.first
-    @items = Item.where("occurs_at < ?", 7.days.from_now)
+    @items = Item.where("occurs_at < ?", 7.days.from_now).order(:occurs_at)
     @topics = Topic.all
   end
 end
