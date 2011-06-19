0fa2ee38dc2b3187d497e45d78651cf9c767b2b4
Erik Michaels-Ober <sferik@gmail.com>
2011-06-19 08:42:24 -0700

    Make user database-driven

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,7 @@
 class MainController < ApplicationController
   def index
+    # TODO: Add authentication logic
+    @user = User.first
     @items = Item.where("occurs_at < ?", 7.days.from_now)
     @topics = Topic.all
   end
